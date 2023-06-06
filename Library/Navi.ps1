26a8639e3ef7773d2277d4a82cbd6e649f0d88b4
WravenDERF <90710167+WravenDERF@users.noreply.github.com>
2023-06-06 12:45:12 -0400

    Update Navi.ps1

--- Library/Navi.ps1
+++ Library/Navi.ps1
@@ -1,22 +1,3 @@
-Function Resolve-WebLinkAlso {
-
-  param(
-    [string]$url,
-    [int]$parallelCount = 10,
-    [int]$iterations = 10
-  )
-
-  foreach -parallel ($x in 1..$parallelCount) {
-    1..$iterations | %{ 
-        $response = curl $url
-        $status = $response.StatusCode
-        "worker $x : iteration $_ : $status"
-        [System.Threading.Thread]::Sleep(500)
-    }
-  }
-
-} 
-
 Function Resolve-WebLink {
 
     PARAM ([string]$WebAddress)
@@ -26,6 +7,35 @@ Function Resolve-WebLink {
     RETURN $HTTP_Response.StatusCode
 
 }
+
+Function Test-WebLink {
+
+    PARAM(
+        [string]$WebAddress,
+        [int]$WorkerCount = 10,
+        [int]$IterationCount = 20
+    )
+    
+    FOR ($Worker = 1; $Worker -le $WorkerCount; $Worker = $Worker + 1) {
+        FOR ($Iteration = 1; $Iteration -le $IterationCount; $Iteration = $Iteration + 1) {
+            $Response = cURL $WebAddress
+            $Status = $Response.StatusCode
+            Write-Host -Object $("Worker $Worker : Iteration $Iteration : $Status")
+            Start-Sleep -Milliseconds 5
+        }
+    }
+
+    #$WebAddress = 'www.google.com'
+    #$HTTP_Request = [System.Net.WebRequest]::Create($WebAddress)
+    #$HTTP_Response = $HTTP_Request.GetResponse()
+    #$HTTP_Response
+
+    #$WebAddress = 'https://iscv-pre.stanfordmed.org/iscv'
+    #$Response = cURL $WebAddress
+    #$Response
+
+}
+
 
 Clear-Host
 Write-Host -Object 'Hello World!'
